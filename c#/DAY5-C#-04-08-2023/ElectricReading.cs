984798f6432a1f0888246d0ed5d16b0c17a930c9
kiruba1003 <141032909+kiruba1003@users.noreply.github.com>
2023-08-04 23:43:16 +0530

    Add files via upload

---
+++ c#/DAY5-C#-04-08-2023/ElectricReading.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace simpleprograms
+{
+    class ElectricReading
+    {
+        private int curreading;
+        string consumer_name, consumer_type;
+        private int consumer_number;
+        private int prev_reading;
+
+        //public void ReadConsumerData()
+        //{
+        //    Console.WriteLine("consumer_number : ");
+        //    consumer_number = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("consumer_name : ");
+        //    consumer_name = Console.ReadLine();
+        //    Console.WriteLine("prev_reading : ");
+        //    prev_reading = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("curreading : ");
+        //    curreading = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("consumer_type : ");
+        //    consumer_type = Console.ReadLine();
+        //}
+        public ElectricReading(int consumer_number, string consumer_name, int prev_reading, int curreading, string consumer_type)
+        {
+            this.Consumer_number = consumer_number;
+            this.Consumer_name = consumer_name;
+            this.Prev_reading = prev_reading;
+            this.Curreading = curreading;
+            this.Consumer_type = consumer_type;
+        }
+
+        public int Consumer_number { get => consumer_number; set => consumer_number = value; }
+        public int Prev_reading { get => prev_reading; set => prev_reading = value; }
+        public int Curreading { get => curreading; set => curreading = value; }
+        public string Consumer_name { get => consumer_name; set => consumer_name = value; }
+        public string Consumer_type { get => consumer_type; set => consumer_type = value; }
+
+        public int CalculateBill()
+        {
+            
+            int billamt = 0;
+            int consumption = Curreading - Prev_reading;
+            if (Consumer_type.Equals("Domestic"))
+            {
+
+                if (consumption <= 100)
+                {
+                    billamt = 0;
+                }
+                else if (consumption > 100 && consumption <= 200)
+                {
+                    billamt = (consumption - 100) * 2;
+                }
+                else if (consumption > 200 && consumption <= 500)
+                {
+                    billamt = (consumption - 100) * 5;
+                }
+                else if (consumption > 500)
+                {
+                    billamt = (consumption - 100) * 10;
+                }
+            }
+            else if (Consumer_type.Equals("Commercial"))
+            {
+                if (consumption <= 100)
+                {
+                    billamt = 10;
+                }
+                else if (consumption > 100 && consumption <= 200)
+                {
+                    billamt = (consumption - 100) * 20;
+                }
+                else if (consumption > 200 && consumption <= 500)
+                {
+                    billamt = (consumption - 100) * 50;
+                }
+                else if (consumption > 500)
+                {
+                    billamt = (consumption - 100) * 100;
+                }
+            }
+            return billamt;
+
+        }
+        //public void DisplayBillDetails()
+        //{
+        //    int billamt = CalculateBill();
+        //    Console.WriteLine($"Bill : " +$"{ consumer_number} { consumer_name } { billamt }");//string interpolation value
+        //    Console.ReadLine();
+        //}
+
+    }
+}
---
+++ c#/DAY5-C#-04-08-2023/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace simpleprograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double num1, num2;
+            //int res;
+            //num1 = Convert.ToDouble(Console.ReadLine());
+            //num2 = Convert.ToDouble(Console.ReadLine());
+            //res =(int)( num1 + num2);
+            //Console.WriteLine("The result is "+res);
+            //Console.ReadLine();
+            //int num, sum = 0,rem,number;
+            //num = Convert.ToInt32(Console.ReadLine());
+            //number = num;
+            //while(num>0)
+            //{
+            //    rem = num % 10;
+            //    sum =rem*rem*rem+sum;
+            //    num = num / 10;
+            //}
+            //if (sum == number)
+            //{
+            //    Console.WriteLine("The result is " + sum);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("not arm");
+            //}
+            //
+
+            //int num, sum = 0;
+            //num = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= num / 2; i++)
+            //{
+            //    if (num % i == 0)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+
+            //if (sum == num)
+            //{
+            //    Console.WriteLine("Perfect Number");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" Not an Perfect Number");
+            //}
+            /*
+            Console.WriteLine("consumer_number : ");
+           int consumer_number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("consumer_name : ");
+            string consumer_name = Console.ReadLine();
+            Console.WriteLine("prev_reading : ");
+            int prev_reading = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("curreading : ");
+            int curreading = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("consumer_type : ");
+            string consumer_type = Console.ReadLine();//string ? means take null valuesconsumer_number, consumer_name, prev_reading, curreading, consumer_type
+            */
+            //ElectricReading electricReading = new ElectricReading(100, "kiruba", 200, 400, "Commercial");
+            //electricReading.ReadConsumerData();
+            //int billamt = electricReading.CalculateBill();
+            //electricReading.DisplayBillDetails();
+            //Console.WriteLine($"Bill : " + $"{ electricReading.Consumer_number}" + $" { electricReading.Consumer_name } " + $"{ billamt }");//string interpolation value
+
+
+
+            Console.WriteLine("acc_name : ");
+            string acc_holder_name = Console.ReadLine();
+            Console.WriteLine("dep_amount ");
+             int dep_amount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("withdraw amount :  ");
+            int withdraw_Amt = Convert.ToInt32(Console.ReadLine());
+
+
+            BankAccount bankAccount =new BankAccount(acc_holder_name);
+            
+            bankAccount.Deposit(dep_amount);
+            bankAccount.Withdraw(withdraw_Amt);
+            bankAccount.Display();
+            Console.ReadLine();
+
+
+        }
+    }
+}
