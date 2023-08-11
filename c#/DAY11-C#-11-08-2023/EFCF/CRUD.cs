df8faa4b6302b0bf1fa6aeed21cce06f230f3898
kiruba1003 <141032909+kiruba1003@users.noreply.github.com>
2023-08-11 14:18:52 +0530

    Add files via upload

---
+++ c#/DAY11-C#-11-08-2023/EFCF/CRUD.cs
@@ -0,0 +1,46 @@
+﻿using EFCF.Context;
+using System;
+using EFCF.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCF
+{
+    internal class CRUD
+    {
+        public void AddNewDept()
+        {
+            using (var context = new EmployeeManagement())
+            {
+                var dept = new Dept()
+                {
+                    Deptno = 10,
+                    Dname = "IT"
+                };
+                context.Depts.Add(dept);
+                context.SaveChanges();
+            }
+        }
+        public void UpdateDept()
+        {
+            var context = new EmployeeManagement();
+            var seldept = context.Depts.FirstOrDefault(d => d.Deptno == 10);
+            seldept.Dname = "CS";
+            context.SaveChanges();
+        }
+        public void DeleteDept()
+        {
+            var context = new EmployeeManagement();                 
+            var seldept = context.Depts.FirstOrDefault(d => d.Deptno == 10);
+
+            context.Depts.Remove(seldept);
+            context.SaveChanges();
+
+        }
+
+
+
+    }
+}
