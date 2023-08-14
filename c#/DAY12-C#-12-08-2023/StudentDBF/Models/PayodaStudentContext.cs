8fde267036bb8d312e04fbdf046e4123cf578c73
kiruba1003 <141032909+kiruba1003@users.noreply.github.com>
2023-08-14 12:28:41 +0530

    Add files via upload

---
+++ c#/DAY12-C#-12-08-2023/StudentDBF/Models/PayodaStudentContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentDBF.Models;
+
+public partial class PayodaStudentContext : DbContext
+{
+    public PayodaStudentContext()
+    {
+    }
+
+    public PayodaStudentContext(DbContextOptions<PayodaStudentContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("data source = .\\SQLEXPRESS; initial catalog = PayodaStudent; integrated security=SSPI; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Rollno);
+
+            entity.Property(e => e.Rollno).ValueGeneratedNever();
+            entity.Property(e => e.Phno).HasColumnName("phno");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ c#/DAY12-C#-12-08-2023/StudentDBF/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentDBF.Models;
+
+public partial class Student
+{
+    public int Rollno { get; set; }
+
+    public string Studentame { get; set; } = null!;
+
+    public string Addr { get; set; } = null!;
+
+    public long Phno { get; set; }
+}
