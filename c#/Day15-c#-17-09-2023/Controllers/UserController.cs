6a393cb1d7cc6ad3d70ca941a5542a541b2c92f8
kiruba1003 <141032909+kiruba1003@users.noreply.github.com>
2023-08-19 08:13:16 +0530

    Add files via upload

---
+++ c#/Day15-c#-17-09-2023/Controllers/UserController.cs
@@ -0,0 +1,72 @@
+﻿using JWT_Auth.Models;
+using JWT_Auth.Services.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public IUser _user;
+        public IToken _tokenGenerator;
+
+        public UserController(IUser user, IToken tokenGenerator)
+        {
+            _user = user;
+            _tokenGenerator= tokenGenerator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<string>> GetUserByUsername(string username)
+        {
+            try
+            {
+                var user = await _user.GetUserByUsername(username);
+                var token = _tokenGenerator.GenerateToken(user.UserName, user.Role);
+                return Ok(token);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+        [HttpPut]
+        public async Task<ActionResult<User>> UpdateUser(string username, User user)
+        {
+            try
+            {
+                var users = await _user.UpdateUser(username, user);
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<List<User>>> DeleteUser(string username)
+        {
+            try
+            {
+                var users = await _user.DeleteUser(username);
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> AddUser(User user)
+        {
+            var users = await _user.AddUser(user);
+            return Ok(users);
+        }
+
+    }
+
+}
---
+++ c#/Day15-c#-17-09-2023/Program.cs
@@ -0,0 +1,45 @@
+using JWT_Auth.Models;
+using JWT_Auth.Services.Interface;
+using JWT_Auth.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<IStudent, StudentService>(); builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PayodaStudentContext>(
+
+ optionsAction: options => options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnection"))
+
+);
+
+builder.Services.AddScoped<IStudent, StudentService>();
+builder.Services.AddScoped<IUser, UserService>();
+builder.Services.AddScoped<IToken, TokenService>();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//follow order 1st authentication ,2nd authorization
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
