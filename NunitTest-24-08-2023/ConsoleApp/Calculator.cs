24bfb2d1fefb8e022b66878875d165868c0d7117
kiruba1003 <141032909+kiruba1003@users.noreply.github.com>
2023-08-27 11:23:10 +0530

    Add files via upload

---
+++ NunitTest-24-08-2023/ConsoleApp/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public  class Calculator
+    {
+        public int add(int num1 ,int num2)
+        {
+            return num1 + num2;
+        }
+        public int sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int divide(int num1, int num2)
+        {
+            return num1/num2;
+        }
+    }
+}
---
+++ NunitTest-24-08-2023/ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+
+Calculator calculator = new Calculator();
+calculator.add(1, 6);
+calculator.sub(3,2);
